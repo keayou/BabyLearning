e2e4061668a1f8217e5cf265e6398b5b4d572fb7
fengke <fengke@sogou-inc.com>
2017-10-27 19:22:49 +0800

    Initial Commit

---
+++ BabyLearning/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BabyLearning
+//
+//  Created by fk on 2017/10/27.
+//  Copyright © 2017年 fk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
